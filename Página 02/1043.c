82e7c010b8098590057c9739ce6f1c65c75a000e
blegora <blegora@users.noreply.github.com>
2017-04-17 22:34:30 -0300

    Update 1043.c
    int abs() -> double fabs()

--- Página 02/1043.c
+++ Página 02/1043.c
@@ -6,11 +6,9 @@ int main() {
     printf("");
     scanf("%lf %lf %lf", &A, &B, &C);
 
-    if (((abs(B - C) < A) && (A < (B + C))) &&
-        ((abs(A - C) < B) && (B < (A + C))) &&
-        ((abs(A - B) < C) && (C < (A + B)))) {
-            P = A + B + C;
-            printf("Perimetro = %.1lf\n", P);
+    if ((fabs(B - C) < A) && (A < (B + C)) && (fabs(A - C) < B) && (B < (A + C)) && (fabs(A - B) < C) && (C < (A + B)) ) {
+        P = A + B + C;
+        printf("Perimetro = %.1lf\n", P);
     } else {
         Area = ((A + B) * C) / 2;
         printf("Area = %.1lf\n", Area);
